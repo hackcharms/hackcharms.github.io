3a88cfbb34ac49f937ad73ebe9d72474b8b0b546
Zubair <zubair.ahamd@credilio.in>
2024-09-19 18:31:15 +0530

    deployment script

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { getOrCreateStore } from "./database.ts";
 import { RouterProvider } from "react-router-dom";
-import Routes from "./routes/index.tsx";
+import Routes from "./routes";
 // getOrCreateStore("todoDB","todoSchema",'id')
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
